4ae3df591a565b0ba9b4c0719940fe7e3dfc988c
sanka01 <sanka1375@gmail.com>
2019-10-29 14:51:33 -0300

    Initial commit

---
+++ src/br/unitins/loja_jogos/controller/PerfilController.java
@@ -0,0 +1,9 @@
+package br.unitins.loja_jogos.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class PerfilController {
+}
---
+++ src/br/unitins/loja_jogos/controller/indexController.java
@@ -0,0 +1,10 @@
+package br.unitins.loja_jogos.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class indexController {
+
+}
